56c77bdb978a9dde1ee9057621d06f7b5c4e73df
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-01-11 17:59:03 +0100

    Added fields to ContactSearchIndex

--- Entity/ContactSearchIndex.php
+++ Entity/ContactSearchIndex.php
@@ -6,5 +6,7 @@ use Librinfo\BaseEntitiesBundle\Entity\SearchIndexEntity;
 
 class ContactSearchIndex extends SearchIndexEntity
 {
-
+    // TODO: this should go in the contact.orm.yml mapping file :
+    //       find a way to override Doctrine ORM YamlDriver and ClassMetadata classes
+    public static $fields = ['name', 'firstname', 'shortname', 'email'];
 }
