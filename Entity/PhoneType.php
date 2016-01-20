8e1c593a4cc94c65a6162b0a041642fa56c37f0e
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-01-20 12:16:09 +0100

    Added Sortable trait to PhoneType entity

--- Entity/PhoneType.php
+++ Entity/PhoneType.php
@@ -2,13 +2,13 @@
 
 namespace Librinfo\CRMBundle\Entity;
 
-use Librinfo\DoctrineBundle\Entity\Traits\BaseEntity;
-use Librinfo\DoctrineBundle\Entity\Traits\Nameable;
 use Librinfo\DoctrineBundle\Entity\Traits\Guidable;
 use Librinfo\DoctrineBundle\Entity\Traits\Labelable;
+use Librinfo\DoctrineBundle\Entity\Traits\Sortable;
 
 class PhoneType
 {
     use Guidable,
-        Labelable;
+        Labelable,
+        Sortable;
 }
