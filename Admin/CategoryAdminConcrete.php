9bf518bded156570f2c3e1089be93933dc4eca56
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-05-04 13:57:25 +0200

    Refactored Treeable forms

--- Admin/CategoryAdminConcrete.php
+++ Admin/CategoryAdminConcrete.php
@@ -7,10 +7,10 @@ use Librinfo\CoreBundle\Admin\Traits\Base as BaseAdmin;
 class CategoryAdminConcrete extends CategoryAdmin
 {
     use BaseAdmin;
-    
+
     protected $datagridValues = array(
         '_page'       => 1,
         '_sort_order' => 'ASC',
         '_sort_by'    => 'sortMaterializedPath',
     );
-}
+    }
---
+++ Entity/Repository/CategoryRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Librinfo\CRMBundle\Entity\Repository;
+
+use Librinfo\DoctrineBundle\Entity\Repository\TreeableRepository;
+
+class CategoryRepository extends TreeableRepository
+{
+    /**
+     * Returns the tree for the form widget
+     *
+     * @param string $rootAlias
+     * @return array
+     */
+    public function getFormTree($rootAlias = 't')
+    {
+        $all = $this->createQueryBuilder($rootAlias)
+            ->andWhere($rootAlias.'.materializedPath NOT LIKE :pattern')
+            ->addOrderBy($rootAlias.'.sortMaterializedPath', 'ASC')
+            ->setParameter('pattern', '/%/%')  # max level = 1
+            ->getQuery()
+            ->execute()
+        ;
+
+        $allRootNodes = array();
+        foreach ($all as $node) if ($node->isRootNode()) {
+            $allRootNodes[] = $node;
+            break;
+        }
+        foreach ($allRootNodes as $root)
+            $root->buildTree($all);
+
+        dump('allRouteNodes', $allRootNodes);
+
+        return $allRootNodes;
+    }
+
+    public function getOrCreateRootNode()
+    {
+        $root_nodes = $this->getRootNodes();
+        if ( count($root_nodes) > 0 )
+            return $root_nodes[0];
+
+        $root = new \Librinfo\VarietiesBundle\Entity\PlantCategory;
+        $root->setName('_ROOT_');
+        $root->setMaterializedPath('');
+        $root->setSortMaterializedPath('');
+        $em = $this->getEntityManager();
+        $em->persist($root);
+        $em->flush();
+        return $root;
+    }
+
+    public function getAllWithoutRootQB($rootAlias = 't')
+    {
+        $qb = $this->createQueryBuilder($rootAlias)
+                ->andWhere("$rootAlias.name != :root_name")
+                ->setParameter(':root_name', '_ROOT_');
+        return $qb;
+    }
+}
