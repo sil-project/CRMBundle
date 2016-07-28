5fb4e3f385ffdb7fc89c4af0c36efcf9c9ee116b
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-07-28 15:13:49 +0200

    Fixed Organism customerCode and supplierCode generators

--- Entity/Repository/OrganismRepository.php
+++ Entity/Repository/OrganismRepository.php
@@ -15,7 +15,7 @@ class OrganismRepository extends EntityRepository
     public function generateCustomerCode()
     {
         $regexp = sprintf('^%s(\d{%d})$', Organism::CC_PREFIX, Organism::CC_LENGTH);
-        $max = $this->createQueryBuilder('c')
+        $res = $this->createQueryBuilder('c')
             ->select("SUBSTRING(c.customerCode, '$regexp') AS code")
             ->andWhere("SUBSTRING(c.customerCode, '$regexp') != ''")
             ->setMaxResults(1)
@@ -23,7 +23,9 @@ class OrganismRepository extends EntityRepository
             ->getQuery()
             ->getScalarResult()
         ;
-        return sprintf("%s%0".Organism::CC_LENGTH."d", Organism::CC_PREFIX, (int)$max + 1);
+        $max = $res ? (int)$res[0]['code'] : 0;
+
+        return sprintf("%s%0".Organism::CC_LENGTH."d", Organism::CC_PREFIX, $max + 1);
     }
 
     /**
@@ -34,7 +36,7 @@ class OrganismRepository extends EntityRepository
     public function generateSupplierCode()
     {
         $regexp = sprintf('^%s(\d{%d})$', Organism::SC_PREFIX, Organism::SC_LENGTH);
-        $max = $this->createQueryBuilder('c')
+        $res = $this->createQueryBuilder('c')
             ->select("SUBSTRING(c.supplierCode, '$regexp') AS code")
             ->andWhere("SUBSTRING(c.supplierCode, '$regexp') != ''")
             ->setMaxResults(1)
@@ -42,8 +44,9 @@ class OrganismRepository extends EntityRepository
             ->getQuery()
             ->getScalarResult()
         ;
+        $max = $res ? (int)$res[0]['code'] : 0;
 
-        return sprintf("%s%0".Organism::SC_LENGTH."d", Organism::SC_PREFIX, (int)$max + 1);
+        return sprintf("%s%0".Organism::SC_LENGTH."d", Organism::SC_PREFIX, $max + 1);
     }
 
 }
