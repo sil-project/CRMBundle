e6e57b9c8817f4efd89c387004a4920f18e20c8e
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-08-03 11:09:23 +0200

    Added DependencyInjection/Compiler/NormalizerCompilerPass.php

---
+++ DependencyInjection/Compiler/NormalizerCompilerPass.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Librinfo\CRMBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class NormalizerCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $service = $container->getDefinition('librinfo.base_entities.listener.normalize');
+        $config = $container->getParameter('librinfo_crm');
+        foreach ($config as $class => $settings) {
+            $class = "Librinfo\\CRMBundle\\Entity\\" . $class;
+            if ( class_exists($class) && !empty($settings['normalize']) ) {
+                $arg = [$class => $settings['normalize']];
+                $service->addMethodCall('addActions', [$arg]);
+            }
+        }
+
+    }
+}
