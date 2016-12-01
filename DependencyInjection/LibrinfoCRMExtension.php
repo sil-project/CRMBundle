ad12790a2c81980c2f2d583c963e9e225ede5949
Baptiste SIMON <beta@e-glop.net>
2016-12-01 18:04:35 +0100

    implementing the new BlastCoreExtension::load() method

--- DependencyInjection/LibrinfoCRMExtension.php
+++ DependencyInjection/LibrinfoCRMExtension.php
@@ -2,13 +2,12 @@
 
 namespace Librinfo\CRMBundle\DependencyInjection;
 
-use Blast\CoreBundle\DependencyInjection\DefaultParameters;
 use Blast\CoreBundle\DependencyInjection\BlastCoreExtension;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Yaml\Yaml;
+
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -23,11 +22,13 @@ class LibrinfoCRMExtension extends BlastCoreExtension implements PrependExtensio
     public function prepend(ContainerBuilder $container)
     {
         // Define Application Circles by parsing circles.yml files in installed Bundles
-
         $circles = [];
         $bundles = $container->getParameter('kernel.bundles');
         foreach ($bundles as $bundleClassName) {
             $rc = new \ReflectionClass($bundleClassName);
+        if ($container->getParameter('kernel.environment') == 'test')
+            $loader->load('datafixtures.yml'); // not defined yet??
+
             $bundleDir = dirname($rc->getFileName());
             $circlesYml = $bundleDir . '/Resources/config/circles.yml';
             if (file_exists($circlesYml))
@@ -37,44 +38,42 @@ class LibrinfoCRMExtension extends BlastCoreExtension implements PrependExtensio
             $container->prependExtensionConfig('librinfo_crm', ['Circle' => ['app_circles' => $circles]]);
             $container->prependExtensionConfig('twig', ['globals' => ['librinfo_app_circles' => $circles]]);
         }
-
     }
 
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function loadSecurity()
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-        $loader->load('admin.yml');
-
-        $container->setParameter('librinfo_crm', $config);
-
+        if (class_exists('\Librinfo\SecurityBundle\Configurator\SecurityConfigurator'))
+            \Librinfo\SecurityBundle\Configurator\SecurityConfigurator::getInstance($container)->loadSecurityYml(__DIR__ . '/../Resources/config/security.yml');
+        return $this;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function loadDataFixtures(ContainerBuilder $container, Loader\FileLoader $loader)
+    {
+        // the fixtures
+        if ($container->getParameter('kernel.environment') == 'test')
+            $loader->load('datafixtures.yml');
+        return $this;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function loadCodeGenerators(ContainerBuilder $container, array $config)
+    {
         // Entity code generators
+        $container->setParameter('librinfo_crm', $config);
         $container->setParameter('librinfo_crm.code_generator.supplier',
             $container->getParameter('librinfo_crm')['code_generator']['supplier']
         );
         $container->setParameter('librinfo_crm.code_generator.customer',
             $container->getParameter('librinfo_crm')['code_generator']['customer']
         );
-
-        if ($container->getParameter('kernel.environment') == 'test')
-        {
-            $loader->load('datafixtures.yml');
-        }
-
-        $this->mergeParameter('blast', $container, __DIR__ . '/../Resources/config');
-
-        if (class_exists('\Librinfo\SecurityBundle\Configurator\SecurityConfigurator'))
-            \Librinfo\SecurityBundle\Configurator\SecurityConfigurator::getInstance($container)->loadSecurityYml(__DIR__ . '/../Resources/config/security.yml');
-
-        $configSonataAdmin = Yaml::parse(
-            file_get_contents(__DIR__ . '/../Resources/config/bundles/sonata_admin.yml')
-        );
-        DefaultParameters::getInstance($container)->defineDefaultConfiguration($configSonataAdmin['default']);
+        return $this;
     }
 }
