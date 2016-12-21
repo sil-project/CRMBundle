b64d6dd3a1d00106c1a2a351929faa782f593ff3
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-12-21 11:01:56 +0100

    Replaced OrganismAdminConcrete by OrganismAdmin

--- Admin/OrganismAdmin.php
+++ Admin/OrganismAdmin.php
@@ -11,78 +11,244 @@
 namespace Librinfo\CRMBundle\Admin;
 
 use Blast\CoreBundle\Admin\CoreAdmin;
-use Doctrine\DBAL\Query\QueryBuilder;
-use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Datagrid\ListMapper;
-use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
+use Blast\CoreBundle\Admin\Traits\HandlesRelationsAdmin;
+use Librinfo\CRMBundle\Entity\Contact;
+use Librinfo\CRMBundle\Entity\ContactPhone;
+use Librinfo\CRMBundle\Entity\Organism;
+use Librinfo\CRMBundle\Entity\Position;
+use Librinfo\CRMBundle\Form\DataTransformer\CustomerCodeTransformer;
+use Librinfo\CRMBundle\Form\DataTransformer\SupplierCodeTransformer;
 use Sonata\AdminBundle\Form\FormMapper;
-use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\CoreBundle\Validator\ErrorElement;
 
 class OrganismAdmin extends CoreAdmin
 {
-    /**
-     * @param DatagridMapper $datagridMapper
-     */
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    use HandlesRelationsAdmin;
+
+    public function getFormTheme()
     {
-        $datagridMapper
-            ->add('url')
-            ->add('administrativeNumber');
+        return array_merge(
+            parent::getFormTheme(),
+            array('LibrinfoCRMBundle:Form:form_admin_fields.html.twig')
+        );
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        parent::configureRoutes($collection);
+
+        // xxxxxxxAction in CRUD controller
+        $collection->add('validateVat');
     }
 
     /**
-     * @param ListMapper $listMapper
+     * @param FormMapper $mapper
      */
-    protected function configureListFields(ListMapper $listMapper)
+    protected function postConfigureFormFields(FormMapper $mapper)
     {
-        $listMapper
-//            ->add('url')
-//            ->add('administrativeNumber')
-//            ->add('categories')
-            ->add('_action', 'actions', array(
-                'actions' => array(
-                    'show'   => array(),
-                    'edit'   => array(),
-                    'delete' => array(),
-                )
-            ));
+        $mapper->get('customerCode')->addViewTransformer(new CustomerCodeTransformer());
+        $mapper->get('supplierCode')->addViewTransformer(new SupplierCodeTransformer());
+    }
+
+    public function preUpdate($object)
+    {
+        parent::preUpdate($object);
+
+    }
+
+    public function prePersist($object)
+    {
+        parent::prePersist($object);
+
+        if ( $object->isIndividual() )
+        {
+            // TODO: different rules for organism name creation in config (eg. "Firstname NAME" or "Name, Firstname"...)
+            $firstname = mb_convert_case($this->getForm()->get('firstname')->getNormData(), MB_CASE_TITLE);
+            $name = mb_strtoupper($this->getForm()->get('name')->getNormData());
+            $object->setName($firstname . " " . $name);
+        }
     }
 
     /**
-     * @param FormMapper $formMapper
+     * @param Organism $organism
      */
-    protected function configureFormFields(FormMapper $formMapper)
+    public function postPersist($organism)
     {
-//        $formMapper
-//            ->add('url')
-//            ->add('administrativeNumber')
-//            ->add('categories', 'librinfo_baseentities_treeablechoice', [
-//                'class'       => 'LibrinfoCRMBundle:Category',
-//                'required'    => false,
-//                'empty_value' => '- - -',
-//                'multiple'    => true
-//            ]);
+        parent::postPersist($organism);
+
+        if ( $organism->isIndividual() )
+        {
+            // Create a new Contact & Position associated to the organism
+            $title = $this->getForm()->get('title')->getNormData();
+            $firstname = $this->getForm()->get('firstname')->getNormData();
+            $name = $this->getForm()->get('name')->getNormData();
+            $contact = new Contact;
+            $contact->setTitle($title);
+            $contact->setFirstname($firstname);
+            $contact->setName($name);
+            $contact->setEmail($organism->getEmail());
+            $contact->setAddress($organism->getAddress());
+            $contact->setZip($organism->getZip());
+            $contact->setCity($organism->getCity());
+            $contact->setCountry($organism->getCountry());
+            $this->getModelManager()->create($contact);
+
+            foreach($organism->getPhones() as $oPhone)
+            {
+                $cPhone = new ContactPhone;
+                $cPhone->setPhoneType($oPhone->getPhoneType());
+                $cPhone->setNumber($oPhone->getNumber());
+                $cPhone->setContact($contact);
+                $this->getModelManager()->create($cPhone);
+            }
+
+            $position = new Position;
+            $position->setOrganism($organism);
+            $position->setContact($contact);
+            $position->setEmail($organism->getEmail());
+            $this->getModelManager()->create($position);
+        }
     }
 
     /**
-     * @param ShowMapper $showMapper
+     * @param Organism $organism
      */
-    protected function configureShowFields(ShowMapper $showMapper)
+    public function preRemove($organism)
     {
-        $showMapper
-            ->add('url')
-            ->add('administrativeNumber')
-            ->add('categories');
+        foreach($organism->getPositions() as $position)
+            $this->getModelManager()->delete($position);
+
+        foreach($organism->getPhones() as $phone)
+            $this->getModelManager()->delete($phone);
+
+        parent::preRemove($organism);
+    }
+
+    public function preBatchAction($actionName, \Sonata\AdminBundle\Datagrid\ProxyQueryInterface $query, array &$idx, $allElements)
+    {
+        parent::preBatchAction($actionName, $query, $idx, $allElements);
     }
 
     /**
-     * {@inheritdoc}
+     * @param ErrorElement $errorElement
+     * @param mixed        $object
+     *
+     * @deprecated this feature cannot be stable, use a custom validator,
+     *             the feature will be removed with Symfony 2.2
      */
-    public function getNewInstance()
+    public function validate(ErrorElement $errorElement, $object)
     {
-        $object = parent::getNewInstance();
-        
-        return $object;
+        $this->validateCustomerCode($errorElement, $object);
+        $this->validateSupplierCode($errorElement, $object);
+    }
+
+    /**
+     * Customer code validator
+     *
+     * @param ErrorElement $errorElement
+     * @param Organism $object
+     */
+    public function validateCustomerCode(ErrorElement $errorElement, $object)
+    {
+        $is_new = empty($object->getId());
+        $code = $object->getCustomerCode();
+
+        if ( empty($code) && $object->isCustomer() ) {
+            $errorElement
+                ->with('customerCode')
+                    ->addViolation('A customer code is required for customers')
+                ->end()
+            ;
+        }
+
+        $registry = $this->getConfigurationPool()->getContainer()->get('blast_core.code_generators');
+        $codeGenerator = $registry->getCodeGenerator(Organism::class, 'customerCode');
+        if ( !empty($code) && !$codeGenerator->validate($code) ) {
+            $msg = 'Wrong format for customer code. It shoud be: ' . $codeGenerator::getHelp();
+            $errorElement
+                ->with('customerCode')
+                    ->addViolation($msg)
+                ->end()
+            ;
+        }
+
+        if ( !empty($code) ) {
+            $valid = true;
+            $organisms = $this->getModelManager()->findBy(
+                 'Librinfo\CRMBundle\Entity\Organism',
+                 ['customerCode' => $code]
+             );
+            if ( $organisms ) {
+                if ( $is_new )
+                    $valid = false;
+                else foreach ( $organisms as $organism ) if ( $organism->getId() != $object->getId() ) {
+                    $valid = false;
+                    break;
+                }
+            }
+            if ( !$valid )
+                $errorElement
+                    ->with('customerCode')
+                        ->addViolation('This customer code is already in use')
+                    ->end()
+                ;
+        }
+
+    }
+
+
+    /**
+     * Supplier code validator
+     *
+     * @param ErrorElement $errorElement
+     * @param Organism $object
+     */
+    public function validateSupplierCode(ErrorElement $errorElement, $object)
+    {
+        $is_new = empty($object->getId());
+        $code = $object->getSupplierCode();
+
+        if ( empty($code) && $object->isSupplier() ) {
+            $errorElement
+                ->with('supplierCode')
+                    ->addViolation('A supplier code is required for suppliers')
+                ->end()
+            ;
+        }
+
+        $registry = $this->getConfigurationPool()->getContainer()->get('blast_core.code_generators');
+        $codeGenerator = $registry->getCodeGenerator(Organism::class, 'supplierCode');
+        if ( !empty($code) && !$codeGenerator->validate($code) ) {
+            $msg = 'Wrong format for supplier code. It shoud be: ' . $codeGenerator::getHelp();
+            $errorElement
+                ->with('supplierCode')
+                    ->addViolation($msg)
+                ->end()
+            ;
+        }
+
+        if ( !empty($code) ) {
+            $valid = true;
+            $organisms = $this->getModelManager()->findBy(
+                 'Librinfo\CRMBundle\Entity\Organism',
+                 ['supplierCode' => $code]
+             );
+            if ( $organisms ) {
+                if ( $is_new )
+                    $valid = false;
+                else foreach ( $organisms as $organism ) if ( $organism->getId() != $object->getId() ) {
+                    $valid = false;
+                    break;
+                }
+            }
+            if ( !$valid )
+                $errorElement
+                    ->with('supplierCode')
+                        ->addViolation('This supplier code is already in use')
+                    ->end()
+                ;
+        }
     }
 
     /**
@@ -93,7 +259,7 @@ class OrganismAdmin extends CoreAdmin
      */
     public static function contactFilterQueryBuilder(ProxyQueryInterface $queryBuilder, $alias, $field, $value)
     {
-        if (!$value['value']) 
+        if (!$value['value'])
             return;
 
         $search = '%' . $value['value'] . '%';
