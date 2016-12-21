1c3dc033c9a398aefc7b4b2335a7b387a51ff402
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-12-21 11:08:41 +0100

    Use OrganismAdmin instead of OrganismAdminConcrete

--- Admin/CustomerAdmin.php
+++ Admin/CustomerAdmin.php
@@ -1,10 +1,18 @@
 <?php
 
+/*
+ * Copyright (C) 2015-2016 Libre Informatique
+ *
+ * This file is licenced under the GNU GPL v3.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Librinfo\CRMBundle\Admin;
 
-use Librinfo\CRMBundle\Admin\OrganismAdminConcrete as BaseOrganismAdminConcrete;
+use Librinfo\CRMBundle\Admin\OrganismAdmin as BaseOrganismAdmin;
 
-class CustomerAdmin extends BaseOrganismAdminConcrete
+class CustomerAdmin extends BaseOrganismAdmin
 {
     protected $baseRouteName = 'admin_librinfo_crm_customer';
     protected $baseRoutePattern = 'librinfo/crm/customer';
@@ -14,7 +22,7 @@ class CustomerAdmin extends BaseOrganismAdminConcrete
         $query = parent::createQuery($context);
 
         $query->andWhere('o.customer = true');
-        
+
         return $query;
     }
 }
