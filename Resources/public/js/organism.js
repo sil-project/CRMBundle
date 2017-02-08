b0fd3d7dafb3771128b9fd39c3ee5a7c5adead4f
Romain <romain.sanchez@libre-informatique.fr>
2017-02-08 11:25:34 +0100

    Code clean up

--- Resources/public/js/organism.js
+++ Resources/public/js/organism.js
@@ -8,14 +8,14 @@ $(document).ready(function(){
     // ********** Individual / Collective
 
     function toggleIndividual(){
-        var $individual_1 = $('#' + prefix + 'isIndividual_1');
-        if( $individual_1.prop('checked') ) {
-            $individual_1.closest('ul').next('.help-block').show();
+        var individual = $('#' + prefix + 'isIndividual_1');
+        if( individual.prop('checked') ) {
+            individual.closest('ul').next('.help-block').show();
             $('#sonata-ba-field-container-' + prefix + 'title').show();
             $('#sonata-ba-field-container-' + prefix + 'firstname').show();
         }
         else {
-            $individual_1.closest('ul').next('.help-block').hide();
+            individual.closest('ul').next('.help-block').hide();
             $('#sonata-ba-field-container-' + prefix + 'title').hide();
             $('#sonata-ba-field-container-' + prefix + 'firstname').hide();
         }
@@ -27,45 +27,51 @@ $(document).ready(function(){
     // ************ Customer Code
 
     // Move things around...
-    var customer_container = $('#sonata-ba-field-container-' + prefix + 'isCustomer');
+    var customerContainer = $('#sonata-ba-field-container-' + prefix + 'isCustomer');
   
-    customer_container.wrap('<div class="form-inline">');
-    customer_container.find('.sonata-ba-field').css({display: 'inline-block', minWidth: '180px'});
-    customer_container.find('label').css('margin-left', '0').css('margin-right', '1em');
-    var customer_code_container = $('#sonata-ba-field-container-' + prefix + 'customerCode');
-      console.log(customer_code_container.find('input'));
-    customer_code_container.find('input').appendTo(customer_container);
-    customer_code_container.find('a').appendTo(customer_container);
-    customer_code_container.find('label').remove();
+    customerContainer.wrap('<div class="form-inline">');
+    customerContainer.find('.sonata-ba-field').css({display: 'inline-block', minWidth: '180px'});
+    customerContainer.find('label').css('margin-left', '0').css('margin-right', '1em');
+    
+    var customerCodeContainer = $('#sonata-ba-field-container-' + prefix + 'customerCode');
+    
+    customerCodeContainer.find('input').appendTo(customerContainer);
+    customerCodeContainer.find('a').appendTo(customerContainer);
+    customerCodeContainer.find('label').remove();
 
     function generateCustomerCode(){
-        var $code = $('input#' + prefix + 'customerCode')
+        var code = $('input#' + prefix + 'customerCode')
         var url = $('a#' + prefix + 'customerCode_generate_code').attr('href');
-        var data = $code.closest('form').serializeArray();
+        var data = code.closest('form').serializeArray();
+        
         $.post(url, data, function(res){
             if (res['error'] !== undefined)
                 alert(res['error']);
             else if (res['code'] !== undefined)
-                $code.val(res['code']);
+                code.val(res['code']);
         });
     }
 
     function toggleCustomerCode(){
-        var $code = $('#' + prefix + 'customerCode');
-        var $link = $('#' + prefix + 'customerCode_generate_code');
+        var code = $('#' + prefix + 'customerCode');
+        var link = $('#' + prefix + 'customerCode_generate_code');
+        
         if( $('#' + prefix + 'isCustomer').prop('checked') ) {
+            
             $('label[for=' + prefix + 'customerCode').addClass('required');
-            if ($code.data('old') !== undefined)
-                $code.val($code.data('old'));
-            $code.show().prop('required', true);
-            $link.show();
-            if ( $code.val().trim() === "" && errors )
+            if (code.data('old') !== undefined)
+                code.val(code.data('old'));
+            
+            code.show().prop('required', true);
+            link.show();
+            
+            if ( code.val().trim() === "" && errors )
                 generateCustomerCode();
         }
         else {
             $('label[for=' + prefix + 'customerCode').removeClass('required');
-            $code.hide().prop('required', false).data('old', $code.val()).val('');
-            $link.hide();
+            code.hide().prop('required', false).data('old', code.val()).val('');
+            link.hide();
         }
     }
 
@@ -75,45 +81,48 @@ $(document).ready(function(){
     // ************ Supplier Code
 
     // Move things around...
-    var supplier_container = $('#sonata-ba-field-container-' + prefix + 'isSupplier');
-    supplier_container.wrap('<div class="form-inline">');
-    supplier_container.find('.sonata-ba-field').css({display: 'inline-block', minWidth: '180px'});
-    supplier_container.find('label').css('margin-left', '0').css('margin-right', '1em');
-    var supplier_code_container = $('#sonata-ba-field-container-' + prefix + 'supplierCode');
-    supplier_code_container.find('input').appendTo(supplier_container);
-    supplier_code_container.find('a').appendTo(supplier_container);
-    supplier_code_container.find('label').remove();
+    var supplierContainer = $('#sonata-ba-field-container-' + prefix + 'isSupplier');
+    supplierContainer.wrap('<div class="form-inline">');
+    supplierContainer.find('.sonata-ba-field').css({display: 'inline-block', minWidth: '180px'});
+    supplierContainer.find('label').css('margin-left', '0').css('margin-right', '1em');
+    
+    var supplierCodeContainer = $('#sonata-ba-field-container-' + prefix + 'supplierCode');
+    
+    supplierCodeContainer.find('input').appendTo(supplierContainer);
+    supplierCodeContainer.find('a').appendTo(supplierContainer);
+    supplierCodeContainer.find('label').remove();
 
     function generateSupplierCode(){
-        var $code = $('input#' + prefix + 'supplierCode')
+        var code = $('input#' + prefix + 'supplierCode')
         var url = $('a#' + prefix + 'supplierCode_generate_code').attr('href');
-        var data = $code.closest('form').serializeArray();
+        var data = code.closest('form').serializeArray();
         $.post(url, data, function(res){
             if (res['error'] !== undefined)
                 alert(res['error']);
             else if (res['code'] !== undefined)
-                $code.val(res['code']);
+                code.val(res['code']);
         });
     }
 
     function toggleSupplierCode(){
-        var $code = $('#' + prefix + 'supplierCode');
-        var $link = $('#' + prefix + 'supplierCode_generate_code');
-        
+        var code = $('#' + prefix + 'supplierCode');
+        var link = $('#' + prefix + 'supplierCode_generate_code');
         
         if( $('#' + prefix + 'isSupplier').prop('checked') ) {
             $('label[for=' + prefix + 'supplierCode').addClass('required');
-            if ($code.data('old') !== undefined)
-                $code.val($code.data('old'));
-            $code.show().prop('required', true);
-            $link.show();
-            if ( $code.val().trim() === "" && errors )
+            
+            if (code.data('old') !== undefined)
+                code.val(code.data('old'));
+            
+            code.show().prop('required', true);
+            link.show();
+            if ( code.val().trim() === "" && errors )
                 generateSupplierCode();
         }
         else {
             $('label[for=' + prefix + 'supplierCode').removeClass('required');
-            $code.hide().prop('required', false).data('old', $code.val()).val('');
-            $link.hide();
+            code.hide().prop('required', false).data('old', code.val()).val('');
+            link.hide();
         }
     }
 
