ef155aa2a3f29dd27102387e50926b5153c29e1b
BlastCI <r.et.d@libre-informatique.fr>
2017-05-18 11:53:51 +0200

    DevKit updates (#8)

---
+++ bin/ci-scripts/create_travis_database_test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+set -ev
+
+# Database creation
+
+###
+### mysql
+###
+
+# (mysql service is started by default by travis for each build instance
+# (mysql travis user is created by travis for each build instance
+mysql -u travis -e 'CREATE DATABASE travis;' -v
+
+###
+### postgresql
+###
+
+# needed in .travis.yml
+#services:
+#  - postgresql
+# or here :  sudo /etc/init.d/postgresql start
+
+# (we try to create a travis user)
+# psql -c 'CREATE USER travis;' -U postgres
+# psql -c 'ALTER ROLE travis WITH CREATEDB;' -U postgres
+
+psql -c 'CREATE DATABASE travis;' -U postgres
+psql -c 'ALTER DATABASE travis OWNER TO travis' -U postgres
+
+
+#psql -U postgres -c "CREATE EXTENSION 'uuid-ossp';"
+
+###
+###
+###
--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -4,10 +4,11 @@ set -ev
 mkdir --parents "${HOME}/bin"
 
 # PHPUnit install
-if [ ${TRAVIS_PHP_VERSION} '<' '5.6' ]; then
-    PHPUNIT_PHAR=phpunit-4.8.phar
-else
+# strange sh operator '<' ...
+if [ ${TRAVIS_PHP_VERSION} '<' '7.0' ]; then
     PHPUNIT_PHAR=phpunit-5.7.phar
+else
+    PHPUNIT_PHAR=phpunit.phar
 fi
 wget "https://phar.phpunit.de/${PHPUNIT_PHAR}" --output-document="${HOME}/bin/phpunit"
 chmod u+x "${HOME}/bin/phpunit"
@@ -15,6 +16,9 @@ chmod u+x "${HOME}/bin/phpunit"
 # Coveralls client install
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
+
+# Ugly hack
+echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 
 # To be removed when this issue will be resolved: https://github.com/composer/composer/issues/5355
 if [ "${COMPOSER_FLAGS}" = '--prefer-lowest' ]; then
