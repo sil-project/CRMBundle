15f19a57382bad4176924c559b2fcbe11f871995
PapsOu <vivien.frasca@libre-informatique.fr>
2017-06-12 14:30:38 +0200

    Add missing use

--- src/Entity/PhoneType.php
+++ src/Entity/PhoneType.php
@@ -5,6 +5,7 @@ namespace Librinfo\CRMBundle\Entity;
 use Blast\BaseEntitiesBundle\Entity\Traits\Guidable;
 use Blast\BaseEntitiesBundle\Entity\Traits\Labelable;
 use Blast\BaseEntitiesBundle\Entity\Traits\Sortable;
+use Blast\BaseEntitiesBundle\Entity\Traits\Stringable;
 
 class PhoneType
 {
