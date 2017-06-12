f5ae2a4506dd50359721f1435e8bbcf8721a32d6
PapsOu <vivien.frasca@libre-informatique.fr>
2017-06-12 14:41:47 +0200

    Removed useless Stringable trait because it is already done in Labelable

--- src/Entity/PhoneType.php
+++ src/Entity/PhoneType.php
@@ -5,12 +5,10 @@ namespace Librinfo\CRMBundle\Entity;
 use Blast\BaseEntitiesBundle\Entity\Traits\Guidable;
 use Blast\BaseEntitiesBundle\Entity\Traits\Labelable;
 use Blast\BaseEntitiesBundle\Entity\Traits\Sortable;
-use Blast\BaseEntitiesBundle\Entity\Traits\Stringable;
 
 class PhoneType
 {
     use Guidable,
         Labelable,
-        Stringable,
         Sortable;
 }
