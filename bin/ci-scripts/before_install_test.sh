b313f4a1175a54295d3b52728da2caf7b1deaf73
PapsOu <vivien.frasca@libre-informatique.fr>
2017-11-14 10:59:20 +0100

    Merge branch 'master' of github.com:sil-project/Platform

--- bin/ci-scripts/before_install_test.sh
+++ bin/ci-scripts/before_install_test.sh
@@ -4,8 +4,12 @@ set -ev
 # check syntax
 find . -name '*.php' -exec php -l {} \;
 
+# Ugly hack
+echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
 composer self-update --stable
 
-
+composer global require --no-interaction cedx/coveralls
+composer global require --no-interaction  phpunit/phpunit 
 
 
--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -3,16 +3,9 @@ set -ev
 
 mkdir --parents "${HOME}/bin"
 
-# Ugly hack
-echo "memory_limit=-1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
-
 composer install --no-interaction --prefer-dist
-composer require --no-interaction --dev phpunit/phpunit 
-#composer update --prefer-dist --no-interaction --prefer-stable
 
+# composer update --prefer-dist --no-interaction --prefer-stable
 
-# Coveralls client install
-wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
-chmod u+x "${HOME}/bin/coveralls"
 
 
