28b08835b5e31808c41145a4b37a5866ed432945
PapsOu <vivien.frasca@libre-informatique.fr>
2017-11-20 15:20:10 +0100

    Update and adapt translations

--- Admin/CircleAdmin.php
+++ Admin/CircleAdmin.php
@@ -32,77 +32,77 @@ class CircleAdmin extends CoreAdmin
     {
         $query = parent::createQuery($context);
 
-        $config = $this->getConfigurationPool()->getContainer()->getParameter('sil_crm');
-
-        if (!isset($config['Circle']['enabled']) || $config['Circle']['enabled'] == false) {
-            return $query;
-        }
-
-        $ra = $query->getRootAliases()[0];
-
-        $query
-            ->addSelect('users')
-            ->leftJoin($ra . '.users', 'users')
-        ;
-
-        if ($config['Circle']['allow_organizations']) {
-            $query
-                ->addSelect('organisms')
-                ->leftJoin($ra . '.organisms', 'organisms')
-            ;
-        }
-        if ($config['Circle']['allow_individuals']) {
-            $query
-                ->addSelect('contacts')
-                ->leftJoin($ra . '.contacts', 'contacts')
-            ;
-        }
-        if ($config['Circle']['allow_positions']) {
-            $query
-                ->addSelect('positions')
-                ->leftJoin($ra . '.positions', 'positions')
-            ;
-        }
-
-        $user = $this->getConfigurationPool()->getContainer()->get('security.token_storage')->getToken()->getUser();
-        if ($user->hasRole('ROLE_SUPER_ADMIN')) {
-            return $query;
-        }
-
-        // we add 3 conditions :
-        // 1. the Circle has no Owner and no Users
-        // 2. ... OR the current user is the Circle owner
-        // 3. ... OR the current user belongs to the circle users
-
-        $expr = $query->expr();
-
-        // 1. the Circle has no Owner and no Users...
-        $subquery1 = $query->getEntityManager()->createQueryBuilder()
-            ->select('u1.id')
-            ->from('Sil\Bundle\CRMBundle\Entity\Circle', 'c1')
-            ->join('c1.users', 'u1')
-            ->where($expr->eq('c1', $ra));
-        $dql1 = $expr->andX(
-                $expr->isNull($ra . '.owner'), $expr->not($expr->exists($subquery1->getDql()))
-        );
-
-        // 2. the current user is the Circle owner
-        $dql2 = $expr->eq($ra . '.owner', ':user2');
-
-        // 3. the current user belongs to the circle users
-        $subquery3 = $query->getEntityManager()->createQueryBuilder()
-            ->select('c3.id')
-            ->from('Sil\Bundle\CRMBundle\Entity\Circle', 'c3')
-            ->join('c3.users', 'u3')
-            ->where($expr->eq('u3', ':user3'))
-            ->andWhere($expr->eq('c3', $ra));
-        $dql3 = $expr->exists($subquery3->getDql());
-
-        $query->andWhere($expr->orX(
-            $dql1, $dql2, $dql3
-        ));
-        $query->setParameter('user2', $user);
-        $query->setParameter('user3', $user);
+        // $config = $this->getConfigurationPool()->getContainer()->getParameter('sil_crm');
+        //
+        // if (!isset($config['Circle']['enabled']) || $config['Circle']['enabled'] == false) {
+        //     return $query;
+        // }
+        //
+        // $ra = $query->getRootAliases()[0];
+        //
+        // $query
+        //     ->addSelect('users')
+        //     ->leftJoin($ra . '.users', 'users')
+        // ;
+        //
+        // if ($config['Circle']['allow_organizations']) {
+        //     $query
+        //         ->addSelect('organisms')
+        //         ->leftJoin($ra . '.organisms', 'organisms')
+        //     ;
+        // }
+        // if ($config['Circle']['allow_individuals']) {
+        //     $query
+        //         ->addSelect('contacts')
+        //         ->leftJoin($ra . '.contacts', 'contacts')
+        //     ;
+        // }
+        // if ($config['Circle']['allow_positions']) {
+        //     $query
+        //         ->addSelect('positions')
+        //         ->leftJoin($ra . '.positions', 'positions')
+        //     ;
+        // }
+        //
+        // $user = $this->getConfigurationPool()->getContainer()->get('security.token_storage')->getToken()->getUser();
+        // if ($user->hasRole('ROLE_SUPER_ADMIN')) {
+        //     return $query;
+        // }
+        //
+        // // we add 3 conditions :
+        // // 1. the Circle has no Owner and no Users
+        // // 2. ... OR the current user is the Circle owner
+        // // 3. ... OR the current user belongs to the circle users
+        //
+        // $expr = $query->expr();
+        //
+        // // 1. the Circle has no Owner and no Users...
+        // $subquery1 = $query->getEntityManager()->createQueryBuilder()
+        //     ->select('u1.id')
+        //     ->from('Sil\Bundle\CRMBundle\Entity\Circle', 'c1')
+        //     ->join('c1.users', 'u1')
+        //     ->where($expr->eq('c1', $ra));
+        // $dql1 = $expr->andX(
+        //         $expr->isNull($ra . '.owner'), $expr->not($expr->exists($subquery1->getDql()))
+        // );
+        //
+        // // 2. the current user is the Circle owner
+        // $dql2 = $expr->eq($ra . '.owner', ':user2');
+        //
+        // // 3. the current user belongs to the circle users
+        // $subquery3 = $query->getEntityManager()->createQueryBuilder()
+        //     ->select('c3.id')
+        //     ->from('Sil\Bundle\CRMBundle\Entity\Circle', 'c3')
+        //     ->join('c3.users', 'u3')
+        //     ->where($expr->eq('u3', ':user3'))
+        //     ->andWhere($expr->eq('c3', $ra));
+        // $dql3 = $expr->exists($subquery3->getDql());
+        //
+        // $query->andWhere($expr->orX(
+        //     $dql1, $dql2, $dql3
+        // ));
+        // $query->setParameter('user2', $user);
+        // $query->setParameter('user3', $user);
 
         return $query;
     }
