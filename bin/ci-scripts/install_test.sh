eef7c0ac3f0adf421ccc8c1c2b47b5a68e98f98c
François GERBES <francois.gerbes@libre-informatique.fr>
2017-12-21 16:42:04 +0100

    [CI] Update install test env (only composer install)

--- bin/ci-scripts/install_test.sh
+++ bin/ci-scripts/install_test.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env sh
 set -ev
 
-mkdir --parents "${HOME}/bin"
-
 composer install --no-interaction --prefer-dist
 
-# composer update --prefer-dist --no-interaction --prefer-stable
+
 
 
 
